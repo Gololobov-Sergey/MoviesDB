6320a8fe11934792eac7ccb648fae64d79fe1e35
Сергій Гололобов <gololobov.serhiy@gmail.com>
2024-03-26 19:55:34 +0200

    Dapper

---
+++ Student-Dapper/Models/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Dapper.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public DateTime BirthDay { get; set; }
+
+        public int GroupId { get; set; }
+
+        //public Group? Group { get; set; }
+
+
+        public int AddressId { get; set; }
+
+        //public Address? Address { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{Id}. {Name} {BirthDay.ToShortDateString()}";
+        }
+    }
+}
---
+++ Student-Dapper/Program.cs
@@ -0,0 +1,82 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Student_Dapper.Models;
+
+namespace Student_Dapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString =
+                "Data Source=TAURUS\\SQLEXPRESS;" +
+                "Initial Catalog=StudentsDB;" +
+                "Integrated Security=True;" +
+                "Connect Timeout=30;" +
+                "Encrypt=False;" +
+                "Trust Server Certificate=False;" +
+                "Application Intent=ReadWrite;" +
+                "Multi Subnet Failover=False";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                {
+                    //string sql = "SELECT * FROM Students";
+
+                    //var students = connection.Query<Student>(sql);
+
+                    //foreach (var s in students)
+                    //{
+                    //    Console.WriteLine(s);
+                    //}
+                }
+
+
+                {
+                    //string insertStudent = "INSERT INTO " +
+                    //    "Students (Name, BirthDay, GroupId, AddressId) " +
+                    //    "VALUES (@Name, @BD, @GrId, @AddrId)";
+
+                    //object[] param = 
+                    //    { new { 
+                    //        Name = "Jonh",
+                    //        BD = "2000-02-15",
+                    //        GrId = 1, 
+                    //        AddrId = 1 
+                    //    } };
+
+                    //int rows = connection.Execute(insertStudent, param);
+
+                    //Console.WriteLine($"Added {rows} rows");
+                }
+
+
+                {
+                    //string updateStudent = "UPDATE Students SET GroupId = 3 WHERE Id = 5";
+
+                    //int rows = connection.Execute(updateStudent);
+
+                    //Console.WriteLine($"Added {rows} rows");
+                }
+
+
+                {
+                    //string sql = "SELECT COUNT(*) FROM Students";
+                    
+                    //var count = connection.ExecuteScalar<int>(sql);
+
+                    //Console.WriteLine(count);
+                }
+
+
+                {
+                    string sql = "SELECT * FROM Students WHERE Id = @Id";
+                    int id  = Convert.ToInt32(Console.ReadLine());
+                    object param =  new { Id = id } ;
+                    var student = connection.QuerySingle<Student>(sql, param);
+                    Console.WriteLine(student);
+                }
+            }
+        }
+    }
+}
