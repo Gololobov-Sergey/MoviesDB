06069e55188f513ae1d06baea961d5d4822896bf
Сергій Гололобов <gololobov.serhiy@gmail.com>
2024-03-28 20:50:14 +0200

    28.03.2024

--- Subscribers/Program.cs
+++ Subscribers/Program.cs
@@ -2,9 +2,10 @@
 using Microsoft.Data.SqlClient;
 using Subscribers.Model;
 using Z.Dapper.Plus;
-using HtmlAgilityPack;
-using System.Data;
 using System.Collections.Generic;
+using System.Data;
+
+
 
 
 namespace Subscribers
@@ -25,8 +26,37 @@ namespace Subscribers
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                string sql = "SELECT * FROM Subscriber WHERE Id = (SELECT Id FROM Country WHERE Name = @Name)";
+                string sql = "SELECT * FROM Subscriber WHERE CountryId = (SELECT Id FROM Country WHERE Name = @Name)";
                 return connection.Query<Subscriber>(sql, new {Name = country});
+            }
+        }
+
+        static DataTable GetSubscrFromCountry(string country)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = "SELECT S.Name AS SubsName, S.Email, S.Gender, C.Name " +
+                    "FROM Subscriber AS S " +
+                    "JOIN Country AS C ON C.Id = S.CountryId " +
+                    "WHERE CountryId = (SELECT Id FROM Country WHERE Name = @Name)";
+                var reader = connection.ExecuteReader(sql, new { Name = country });
+                DataTable table = new DataTable();
+                table.Load(reader);
+                return table;
+            }
+        }
+
+        static DataTable GetCountSubscrFromCountry()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = "SELECT C.Name AS 'Country', COUNT(S.CountryId) AS 'Count' " +
+                    "FROM Subscriber AS S JOIN Country AS C ON S.CountryId = C.Id " +
+                    "GROUP BY C.Name ORDER BY 2 DESC";
+                var reader = connection.ExecuteReader(sql);
+                DataTable table = new DataTable();
+                table.Load(reader);
+                return table;
             }
         }
 
@@ -34,18 +64,33 @@ namespace Subscribers
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                string sql = "SELECT * FROM Product AS P" +
-                             "JOIN SpecialOfer AS SO ON P.SpecialOferId = SO.Id" +
-                              "WHERE P.CategoryId = " +
-                                 "(SELECT Id FROM Category WHERE Name = @Category) " +
-                              " AND ('@Date' BETWEEN SO.DateStart AND SO.DateEnd)";
+                string sql = "SELECT * FROM Product AS P " +
+                    "JOIN SpecialOfer AS SO ON P.SpecialOferId = SO.Id " +
+                    "WHERE P.CategoryId = (SELECT Id FROM Category WHERE Name = @Category) " +
+                    "AND (@Date BETWEEN SO.DateStart AND SO.DateEnd)";
 
-                return connection.Query<Product>(sql, new { Category = category, Date = date.ToShortDateString() });
+                return connection.Query<Product>(sql, new { Category = category, Date = date.ToString("yyyy-MM-dd") });
             }
         }
 
         static void Main(string[] args)
         {
+            //using (SqlConnection connection = new SqlConnection(connectionString))
+            //{
+            //    var list = new List<Subscriber>();
+            //    list.Add(new Subscriber() { Name = "Alice", BirthDay = new DateTime(1990, 5, 15), Gender = false, Email = "alice@example.com", CountryId = 1 });
+            //    list.Add(new Subscriber() { Name = "Bob", BirthDay = new DateTime(1985, 10, 25), Gender = true, Email = "bob@example.com", CountryId = 2 });
+            //    list.Add(new Subscriber() { Name = "Charlie", BirthDay = new DateTime(1988, 3, 8), Gender = true, Email = "charlie@example.com", CountryId = 3 });
+            //    list.Add(new Subscriber() { Name = "David", BirthDay = new DateTime(1995, 7, 12), Gender = true, Email = "david@example.com", CountryId = 1 });
+            //    list.Add(new Subscriber() { Name = "Eve", BirthDay = new DateTime(1992, 9, 20), Gender = false, Email = "eve@example.com", CountryId = 2 });
+            //    list.Add(new Subscriber() { Name = "Frank", BirthDay = new DateTime(1983, 12, 30), Gender = true, Email = "frank@example.com", CountryId = 3 });
+            //    list.Add(new Subscriber() { Name = "Grace", BirthDay = new DateTime(1998, 2, 5), Gender = false, Email = "grace@example.com", CountryId = 1 });
+            //    list.Add(new Subscriber() { Name = "Henry", BirthDay = new DateTime(1980, 6, 18), Gender = true, Email = "henry@example.com", CountryId = 2 });
+            //    list.Add(new Subscriber() { Name = "Ivy", BirthDay = new DateTime(1993, 11, 10), Gender = false, Email = "ivy@example.com", CountryId = 3 });
+            //    list.Add(new Subscriber() { Name = "Jack", BirthDay = new DateTime(1987, 4, 22), Gender = true, Email = "jack@example.com", CountryId = 1 });
+
+            //    connection.BulkInsert(list);
+            //}
 
             //var subs = GetSubscriberFromCountry("Country 1");
             //foreach (Subscriber s in subs)
@@ -54,11 +99,33 @@ namespace Subscribers
             //}
 
 
-            var products = GetSpecialProductFromDate("Category 1", new DateTime(2024, 03, 28));
-            foreach (var p in products)
-            {
-                Console.WriteLine($"{p.Name}");
-            }
+            //var products = GetSpecialProductFromDate("Category 2", new DateTime(2024, 03, 28));
+
+            //foreach (var p in products)
+            //{
+            //    Console.WriteLine($"{p.Name}");
+            //}
+
+            //var products = GetSubscrFromCountry("Country 1");
+            //products.PrintList((obj, row, column) =>
+            //{
+            //    if (obj == DBNull.Value)
+            //        return null;
+
+            //    if (column.ColumnName == "Gender")
+            //    {
+            //        return (bool)obj ? "M" : "F";
+            //    }
+
+            //    if (column.ColumnName == "BirthDay")
+            //        return ((DateTime)obj).ToShortDateString();
+
+            //    return obj.ToString();
+            //});
+
+
+            var table = GetCountSubscrFromCountry();
+            table.Print();
 
             //using (SqlConnection connection = new SqlConnection(connectionString))
             //{
